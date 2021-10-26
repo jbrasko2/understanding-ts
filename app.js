d091cf0d7a0b021d3d9ebf8cc6bb7b82bb7ec628
jbrasko <jbrasko0508@gmail.com>
2021-10-26 16:31:45 -0500

    compile

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ var add = function (n1, n2) {
 };
 var number1 = '5';
 var number2 = 10;
+var person = {
+    name: 'Max',
+    age: 30
+};
+var personCorrect = {
+    name: 'Max',
+    age: 30
+};
+var favoriteActivities;
+favoriteActivities = ['Baseball', 1];
 console.log(add(number1, number2));
+console.log(person);
